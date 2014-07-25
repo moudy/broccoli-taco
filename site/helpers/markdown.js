6dbfa5f3793deeb6af9023fafae5017cc0eeef00
Moudy <moudy.elkammash@gmail.com>
2014-07-25 09:56:02 -0400

    WIP - more docs

--- site/helpers/markdown.js
+++ site/helpers/markdown.js
@@ -21,7 +21,7 @@ module.exports = function (value) {
   if ('string' === typeof value) {
     input = fs.readFileSync(path.join('site/pages', value)+'.md').toString();
   } else if ('object' === typeof value) {
-    input = value.fn(this);
+    input = value.fn(this).trim();
   }
   return new Handlebars.SafeString(marked(input));
 };
---
+++ site/pages/data.js
@@ -0,0 +1,9 @@
+module.exports = {
+  tagline: 'Static Sites Powered by Broccoli'
+, subTagline: 'Perfect for any site that needs to pull in external data at build time.'
+, checklistItems: [
+    'Dynamicaly generated pages based on data'
+  , 'Handlebars templates + layouts, partials &amp; helpers'
+  , 'SCSS, Browserify &amp; asset fingerprinting'
+  ]
+};
